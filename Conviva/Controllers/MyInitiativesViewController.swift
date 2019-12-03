f54d399a0fcb9b7f5bd189490c6bf10332b8453f
Joyce <joyce.sclimaco@gmail.com>
2019-12-03 13:38:23 -0300

    nem sei mais, irmao

---
+++ Conviva/Controllers/MyInitiativesViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MyInitiativesViewController.swift
+//  Conviva
+//
+//  Created by Joyce Simão Clímaco on 03/12/19.
+//  Copyright © 2019 Gabriel Ferreira. All rights reserved.
+//
+
+import UIKit
+
+class MyInitiativesTableViewController: UITableViewController {
+
+}
