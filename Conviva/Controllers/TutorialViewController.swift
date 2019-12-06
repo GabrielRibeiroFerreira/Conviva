ad24724ce8fd63d706b5cf6d71f7bad474d50968
Gabriel Ferreira <ferreira.ribeirog@gmail.com>
2019-12-06 12:28:52 -0300

    tutorial pronto
    uma classe e boarding art

---
+++ Conviva/Controllers/TutorialViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TutorialViewController.swift
+//  Conviva
+//
+//  Created by Gabriel Ferreira on 04/12/19.
+//  Copyright © 2019 Gabriel Ferreira. All rights reserved.
+//
+
+import UIKit
+
+class TutorialViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
