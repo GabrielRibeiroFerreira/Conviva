38061b01f07b3ba74aa01f310fdce900a93ed25c
Gabriel Ferreira <ferreira.ribeirog@gmail.com>
2019-12-06 12:29:30 -0300

    vacilei no anterior

--- Conviva/Controllers/TutorialViewController.swift
+++ Conviva/Controllers/TutorialViewController.swift
@@ -9,14 +9,26 @@
 import UIKit
 
 class TutorialViewController: UIViewController {
-
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        Setup.setupViewController(self)
+        Setup.setupButton(self.button1, withText: "Avançar")
+        Setup.setupButton(self.button2, withText: "Começar")
     }
     
 
+    @IBAction func nextPage(_ sender: Any) {
+        let pageWidth : CGFloat = self.view.frame.width
+        let contentOffset : CGFloat = self.scrollView.contentOffset.x
+        let slideToX = contentOffset + pageWidth
+        
+        self.scrollView.scrollRectToVisible(CGRect(x:slideToX, y:0, width:pageWidth, height:self.scrollView.frame.height), animated: true)
+    }
     /*
     // MARK: - Navigation
 
